587982ac17a7e97dc37593c1ccfdb0c527ca0fe3
Indu2003 <97019909+Indu2003@users.noreply.github.com>
2022-03-29 12:06:44 +0000

    p7done

--- p7final.c
+++ p7final.c
@@ -1,21 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 struct _fraction
 {
-  int num,den;
+int num,den;
 };
-typedef struct _fraction Fraction;
-  int gcd(int a,int b)
-  {
-    int t;
-    while (b != 0)
-      {
-        t = b;
-        b = a%b;
-        a = t;
-      }
-    return a;
+typedef struct _fraction fraction;
+int find_gcd(int a,int b){
+  int min=a;
+  if(b<min)
+    min=b;
+  for(int i=1;i<=min;i--){
+    if(a%i==0 && b%i==0)
+      return i;
+  }
 }
-int main()
-{
-  printf("%d\n",gcd(16,24));
+fraction input_fraction(fraction *f1,fraction *f2){
+  printf("enter the numerator and denomintor\n");
+  scanf("%d%d%d%d",&f1->num,&f1->den,&f2->num,&f2->den);  f3->den=f1.den*f2.den);
+  f3->num=f1.num*f2.den+f2.num*f1.den;
+  int gcd=find_gcd(f3->num,f3->den);
+  f3->num=f3->num/gcd;
+  f3->den=f3->den/gcd;
+}
+void output(fraction f1,fraction f2,fraction f3){
+  printf("the sum of %d/%d and %d/%d is %d/%d ",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den);
+}
+int main(){
+  fraction f1,f2,f3;
+  input_fraction(&f1,&f2);
+  add_fraction(f1,f2,&f3);
+  output(f1,f2,f3);
+  return 0;
 }
