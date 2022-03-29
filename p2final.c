78479533cf82c41587416650ff4e25787c379c7a
Indu2003 <97019909+Indu2003@users.noreply.github.com>
2022-03-29 11:43:13 +0000

    p2 done

--- p2final.c
+++ p2final.c
@@ -1,1 +1,46 @@
 #include<stdio.h>
+struct _fraction
+{
+  int num,den;
+};
+typedef struct _fraction fraction;
+fraction input_fraction()
+{
+  fraction f;
+  printf("enter fraction\n");
+  scanf("%d %d",&f.num, &f.den);
+  return f;
+}
+fraction smallest_fraction(fraction f1,fraction f2,fraction f3)
+{
+  int e = f1.den * f2.den * f3.den;
+  int a, b, c;
+  a = f1.num * e/f1.den;   
+  b = f2.num * e/f2.den;     
+  c = f3.num * e/f3.den;     
+  if( a<b && a<c)
+  {
+    return f1;
+  }
+  else if(b<c)
+  {
+    return f2;
+  }
+  else{
+    return f3;
+  }
+}
+void output(fraction f1,fraction f2,fraction f3,fraction smallest)
+{
+  printf("the smallest of the three fractions is %d/%d",smallest.num,smallest.den);
+}
+int main()
+{
+  fraction f1,f2,f3,smallest;
+  f1=input_fraction();
+  f2=input_fraction();
+  f3=input_fraction();
+  smallest=smallest_fraction(f1,f2,f3);
+  output(f1,f2,f3,smallest);
+  return 0;
+}
